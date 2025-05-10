ede9f316aebe37c86b4dc4c2411e8eea3a53a7ac
kevinst44 <santiagoduque781@gmail.com>
2025-05-10 14:43:16 -0500

    se actualizaron rutas

--- src/components/Salon.jsx
+++ src/components/Salon.jsx
@@ -21,7 +21,7 @@ export default function Salon(){
           setIsLoading(true);
       
           try {
-            const response = await fetch('http://localhost:4000/palmas/palmasbot', {
+            const response = await fetch('https://palmas-back.vercel.app/palmas/palmasbot', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
@@ -75,7 +75,7 @@ export default function Salon(){
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('http://localhost:4000/palmas/salon', {
+                const response = await fetch('https://palmas-back.vercel.app/palmas/salon', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
